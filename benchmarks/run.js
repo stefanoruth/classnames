2607d36cd33b6af3766f22604461f3fc98ae766e
Stefano Ruth <stefano6262@gmail.com>
2020-12-17 14:29:51 +0100

    Update run.js

--- benchmarks/run.js
+++ benchmarks/run.js
@@ -3,7 +3,6 @@ const { css } = require('../dist/index.cjs')
 
 const suite = new Benchmark.Suite()
 
-// add tests
 suite
     .add('Generating classNames', function () {
         css('a', 'b c', null, undefined, 'd', 1)
@@ -12,16 +11,3 @@ suite
         console.log(String(event.target))
     })
     .run()
-// .add('String#indexOf', function () {
-//     'Hello World!'.indexOf('o') > -1
-// })
-// .add('String#match', function () {
-//     !!'Hello World!'.match(/o/)
-// })
-// // add listeners
-//
-// .on('complete', function () {
-//     console.log('Fastest is ' + this.filter('fastest').map('name'))
-// })
-// // run async
-// .run({ async: true })
